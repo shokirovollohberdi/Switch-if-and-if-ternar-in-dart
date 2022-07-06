20b358252d3462150d0d9af99792e5864741e5cf
Ollohberdi <85603736+shokirovollohberdi@users.noreply.github.com>
2022-07-06 11:01:16 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,66 @@
+void main() {
+
+  
+ /* 
+	bool a = 10==10;
+  
+if else oparations
+  String b = "";
+  if(a){
+    b = "true";
+  }else{
+    b = "false";
+  }
+  print(b);
+*/
+
+
+
+
+  
+  /* if ternar
+
+bool a = 10==9;
+ String  b  = a ? "A is True" : "A is False";
+  print(b);
+  
+*/
+
+
+
+  
+  /*   Switch
+  int weekday = 1;
+
+  switch(weekday){
+      case 1:
+      print("Monday");
+      break;
+
+      case 2:
+      print("Tuesday");
+      break;
+
+      case 3:
+      print("Wednesday");
+      break;
+
+      case 4:
+      print("Thursday");
+      break;
+
+      case 5:
+      print("Friday");
+      break;
+
+      case 6:
+      print("Saturdan");
+      break;
+
+       case 7:
+      print("Sunday");
+      break;
+  }
+  */
+  
+}
